473a8375028f604418215a286a6ee4f2213fd066
CaptainFlint1715 <chanseshirley123@gmail.com>
2024-04-11 15:19:15 -0500

    code for profile route under homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -15,7 +15,6 @@ router.get('/', async (req, res) => {
         });
 
         const posts = postData.map((post) => post.get({ plain: true }))
-
         res.render('homepage', { posts })
     } catch (err) {
         res.status(500).json(err);
@@ -37,7 +36,6 @@ router.get('/post/:id', async (req, res) => {
 
         const post = postData.get({ plain: true });
         res.render('post', { ...post });
-
     } catch (err) {
         res.status(500).json(err)
     }
@@ -45,4 +43,20 @@ router.get('/post/:id', async (req, res) => {
 
 
 // TODO: route for user profile
+router.get('/profile', withAuth, async (req,res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Post }],
+        })
 
+        const user = userData.get({ plain: true });
+
+        res.render('profile', {
+            ...user,
+            logged_in: true
+        });
+    } catch(err) {
+        res.status(500).json(err);
+    }
+})
